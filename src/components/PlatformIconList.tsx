b9e8d0e24c401278c9aa4a59081d96d6fa3797c8
Ansar Muhajir <ansarmuhajir96@gmail.com>
2025-10-06 21:54:22 +0300

    Filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -16,6 +16,7 @@ import { IconType } from "react-icons";
 interface Props {
   platforms: Platform[];
 }
+
 const PlatformIconList = ({ platforms }: Props) => {
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
@@ -32,7 +33,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
