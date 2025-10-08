d3933a33af8e872101931ecd5e0272342ee5fbcf
Ansar Muhajir <ansarmuhajir96@gmail.com>
2025-10-08 10:29:45 +0300

    Ship the genres with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGames = () => useData<Genre>("/genres");
+const useGames = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGames;
